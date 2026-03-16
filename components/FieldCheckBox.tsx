3b5f2690c0a1bb0f8bfaecc13f4e03a4e2b6ba56
Khunthynne <khunthynne@gmail.com>
2026-03-17 03:09:39 +0700

    feat: Introduce FieldCheckBox component for Tanstack React Form, providing a styled checkbox input with validation.

--- components/FieldCheckBox.tsx
+++ components/FieldCheckBox.tsx
@@ -3,7 +3,7 @@ import { useFieldContext } from "../hooks";
 
 import type { WithClassNames, LabelDescription, ValidateProps } from "./type";
 
-import { Field, FieldContent, FieldGroup, FieldLabel } from "@components/ui/field";
+import { Field, FieldContent, FieldDescription, FieldLabel } from "@components/ui/field";
 import { cn } from "@components/ui/utils";
 import { Checkbox } from "@components/custom/checkbox";
 import FieldErrorMessage from "./shared/FieldErrorMessage";
@@ -11,20 +11,49 @@ import FieldErrorMessage from "./shared/FieldErrorMessage";
 type FieldSelectProps = LabelDescription &
   React.ComponentProps<typeof Checkbox> & {
     validate?: ValidateProps;
+    field?: React.ComponentProps<typeof Field>;
+    description?: string;
   } & WithClassNames<"label" | "description" | "selectTriger" | "field" | "validate">;
 
-export default function FieldCheckBox({ label, classNames, validate, className, ...checkbox }: FieldSelectProps) {
+export default function FieldCheckBox({
+  label,
+  classNames,
+  field: fieldProps,
+  validate,
+  description,
+  className,
+  ...checkbox
+}: FieldSelectProps) {
   const { form, name } = useFieldContext<string[]>();
   const field = useField({ mode: "array", name, form });
+  const value = useStore(field.store, state => state.value);
   const errors = useStore(field.store, state => state.meta.errors);
   // const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
   const isInvalid = errors.length > 0;
 
   return (
-    <Field data-invalid={isInvalid} className={cn(``, className, classNames?.field)}>
-      <Checkbox {...checkbox} id={field.name} className={cn(`peer cursor-pointer`)} />
+    <Field
+      data-invalid={isInvalid}
+      className={cn(``, className, classNames?.field)}
+      orientation={"horizontal"}
+      {...fieldProps}
+    >
+      <Checkbox
+        {...checkbox}
+        id={field.name}
+        className={cn(`peer cursor-pointer`)}
+        defaultChecked={value}
+        onCheckedChange={checked => {
+          field.handleChange(checked as boolean);
+        }}
+      />
       <FieldContent>
-        <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+        {label && (
+          <FieldLabel htmlFor={field.name} className={cn(`font-normal`, classNames?.label)}>
+            {label}
+          </FieldLabel>
+        )}
+        {description && <FieldDescription className={cn(classNames?.description)}>{description}</FieldDescription>}
         <FieldErrorMessage className={cn(classNames?.validate)} {...validate} />
       </FieldContent>
     </Field>
